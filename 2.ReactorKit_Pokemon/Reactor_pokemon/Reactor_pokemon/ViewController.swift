f301828a415d705b5c92b14f2051284b2388a128
Ruel <c0407h@gmail.com>
2024-10-29 12:24:40 +0900

    Init] Project Init

---
+++ 2.ReactorKit_Pokemon/Reactor_pokemon/Reactor_pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reactor_pokemon
+//
+//  Created by Chung Wussup on 10/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
